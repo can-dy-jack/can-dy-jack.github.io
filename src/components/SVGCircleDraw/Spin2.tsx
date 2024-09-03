fe844e1c4ef30104e712e94cddb5c4ea47b24414
can-dy-jack <jjack_chen3@163.com>
2024-09-03 17:32:55 +0800

    更新文章、组件

---
+++ src/components/SVGCircleDraw/Spin2.tsx
@@ -0,0 +1,33 @@
+import "./spin2.css";
+
+export default function Spin2() {
+  return (
+    <div
+      style={{
+        height: "400px",
+        width: "400px",
+      }}
+    >
+      <svg
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
+        viewBox="0 0 800 800"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle
+          className="spin2"
+          cx="400"
+          cy="400"
+          fill="none"
+          r="200"
+          stroke-width="50"
+          stroke="#E3876F"
+          strokeDasharray="189 1400"
+          strokeLinecap="round"
+        />
+      </svg>
+    </div>
+  );
+}
--- src/components/SVGCircleDraw/index.tsx
+++ src/components/SVGCircleDraw/index.tsx
@@ -3,129 +3,235 @@ import style from "./index.module.css";
 import { Highlight } from "prism-react-renderer";
 import { usePrismTheme } from "@docusaurus/theme-common";
 import { useState } from "react";
-import Slider from 'react-rangeslider';
-import 'react-rangeslider/lib/index.css'
+import Slider from "react-rangeslider";
+import "react-rangeslider/lib/index.css";
 
 export default function SVGCircleDraw({
-  strokeWidth,
-  radius,
-  color,
-  dash,
-  code,
-  showSDR = false
+  showSW = false,
+  showRadius = false,
+  showStroke = false,
+  showSDR = false,
+  isRound = false,
+  showOffset = false,
+  showAnimate = false,
+  isDashAnimate = false,
 }: {
-  strokeWidth?: number;
-  radius?: number;
-  color?: string;
-  dash?: number;
-  code?: string;
-  showSDR?: boolean
+  showSDR?: boolean;
+  showSW?: boolean;
+  showRadius?: boolean;
+  showStroke?: boolean;
+  isRound?: boolean;
+  showOffset?: boolean;
+  showAnimate?: boolean;
+  isDashAnimate?: boolean;
 }) {
   const prismTheme = usePrismTheme();
 
   const [state, setState] = useState({
-    sw: 20,
-    radius: 100,
+    sw: 10,
+    radius: 50,
     stroke: "#f0788c",
-    sDR: 504
+    sDR: 100,
+    offset: 0,
+    speed: 1.5,
+    easing: "linear",
   });
-  const changeSW = value => {
-    setState(state => ({ ...state, sw: value }));
-  }
-  const changeRadius = value => {
-    setState(state => ({ ...state, radius: value }));
-  }
-  const changeStroke = e => {
-    setState(state => ({ ...state, stroke: e.target.value }));
-  }
+  const changeSW = (value) => {
+    setState((state) => ({ ...state, sw: value }));
+  };
+  const changeRadius = (value) => {
+    setState((state) => ({ ...state, radius: value }));
+  };
+  const changeStroke = (e) => {
+    setState((state) => ({ ...state, stroke: e.target.value }));
+  };
+  const changeSDR = (value) => {
+    setState((state) => ({ ...state, sDR: value }));
+  };
+  const changeOffset = (value) => {
+    setState((state) => ({ ...state, offset: value }));
+  };
 
   const SVGDOM = (
-    <svg viewBox="0 0 800 800" xmlns="http://www.w3.org/2000/svg">
+    <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
       <circle
-        cx="400"
-        cy="400"
+        cx="100"
+        cy="100"
         fill="none"
         r={state.radius}
         strokeWidth={state.sw}
         stroke={state.stroke}
+        strokeDasharray={showSDR ? state.sDR + " 315" : ""}
+        strokeLinecap={isRound ? "round" : "inherit"}
+        strokeDashoffset={showOffset ? state.offset : 0}
+        style={
+          showAnimate
+            ? {
+                transformOrigin: "center",
+                // animation: "spin 4s ease-in infinite",
+                animation: `${isDashAnimate ? "dash-spin" : "spin"} ${
+                  state.speed
+                }s ${state.easing} infinite`,
+                animationDirection: isDashAnimate ? "alternate" : "normal",
+              }
+            : {}
+        }
+        className="spin"
       />
     </svg>
   );
+
+  let styleStr = "";
+  if (isDashAnimate) {
+    styleStr = `<style>
+  @keyframes dash-spin {
+    from {
+      stroke-dasharray: 0 315;
+    }
+    to {
+      stroke-dasharray: 315 400;
+    }
+  }
+  .spin {
+    animation: dash-spin ${state.speed}s ${state.easing} infinite;
+    animation-direction: alternate;
+  }
+</style>`;
+  } else if (showAnimate) {
+    styleStr = `<style>
+  @keyframes spin {
+    to {
+      transform: rotate(360deg);
+    }
+  }
+
+  .spin {
+    transform-origin: center;
+    animation: spin ${state.speed}s ${state.easing} infinite;
+  }
+</style>`;
+  }
+  const svgStr =
+    `r = "${state.radius}" strokeWidth = "${state.sw}" stroke = "${state.stroke}"` +
+    `${showSDR ? `\n    stroke-dasharray="${state.sDR} 315"` : ""}` +
+    `${isRound ? `\n    stroke-linecap="round"` : ""}` +
+    `${
+      showOffset || isDashAnimate
+        ? `\n    stroke-dashoffset="${state.offset}"`
+        : ""
+    }` +
+    `${showAnimate ? `\n    class="spin"` : ""}`;
 
   return (
     <div className={style.svgBox}>
       <div className={style.svgTop}>
         <div className={style.svgShow}>{SVGDOM}</div>
         <div className={style.svgTools}>
-          <div className={style.sw}>
-            <label htmlFor="StrokeWidth">Stroke width</label>
-            <br />
-            {/* <input
-              className={style.input}
-              type="range"
-              id="StrokeWidth"
-              name="StrokeWidth"
-              min="2"
-              max="200"
-              // value={state.sw}
-              defaultValue={20}
-              step="1"
-              onChange={changeSW}
-            /> */}
-            <Slider
-              min={2}
-              max={200}
-              step={1}
-              value={state.sw}
-              onChange={changeSW}
-            />
-          </div>
-          <div className={style.radius}>
-            <label htmlFor="radius">Radius</label>
-            <br />
-            {/* <input
-              className={style.input}
-              type="range"
-              id="radius"
-              name="radius"
-              min="2"
-              max="375"
-              defaultValue={100}
-              step="1"
-              onChange={changeRadius}
-            /> */}
-            <Slider
-              min={2}
-              max={375}
-              step={1}
-              value={state.radius}
-              onChange={changeRadius}
-            />
-          </div>
-          <div className={style.stroke}>
-            <label htmlFor="stroke">stroke: </label>
-            <input
-              type="color"
-              id="stroke"
-              name="stroke"
-              value={state.stroke}
-              // defaultValue={"#f0788c"}
-              onChange={changeStroke}
-            />
-          </div>
+          {showSW && (
+            <div className={style.sw}>
+              <span>Stroke width</span>
+              <Slider
+                min={1}
+                max={80}
+                step={1}
+                value={state.sw}
+                onChange={changeSW}
+              />
+            </div>
+          )}
+          {showRadius && (
+            <div className={style.radius}>
+              <span>Radius</span>
+              <Slider
+                min={1}
+                max={90}
+                step={1}
+                value={state.radius}
+                onChange={changeRadius}
+              />
+            </div>
+          )}
+          {showStroke && (
+            <div className={style.stroke}>
+              <label htmlFor="stroke">stroke: </label>
+              <input
+                type="color"
+                id="stroke"
+                name="stroke"
+                value={state.stroke}
+                // defaultValue={"#f0788c"}
+                onChange={changeStroke}
+              />
+            </div>
+          )}
+          {showSDR && (
+            <div className={style.sdr}>
+              <span>stroke-dasharray: </span>
+              <Slider
+                min={0}
+                max={315}
+                step={1}
+                value={state.sDR}
+                onChange={changeSDR}
+              />
+            </div>
+          )}
+          {showOffset && (
+            <div className={style.offset}>
+              <span>stroke-dashoffset: </span>
+              <Slider
+                min={-300}
+                max={300}
+                step={1}
+                value={state.offset}
+                onChange={changeOffset}
+              />
+            </div>
+          )}
+          {showAnimate && (
+            <div className={style.speed}>
+              <span>Animation speed: </span>
+              <Slider
+                min={0.2}
+                max={10}
+                step={0.1}
+                value={state.speed}
+                onChange={(value) =>
+                  setState((state) => ({
+                    ...state,
+                    speed: Math.round(value * 10) / 10,
+                  }))
+                }
+              />
+            </div>
+          )}
+          {showAnimate && (
+            <div className={style.aesing}>
+              <span>Easing: </span>
+              <select
+                name="easing"
+                defaultValue={state.easing}
+                onChange={(e) =>
+                  setState((state) => ({ ...state, easing: e.target.value }))
+                }
+              >
+                <option value="linear">linear</option>
+                <option value="ease-in">ease-in</option>
+                <option value="ease-out">ease-out</option>
+                <option value="ease-in-out">ease-in-out</option>
+              </select>
+            </div>
+          )}
         </div>
       </div>
       <div className={style.svgCode}>
         <Highlight
-          code={`<svg viewBox="0 0 800 800" xmlns="http://www.w3.org/2000/svg">
-  <circle cx="400" cy="400" fill="none"
-    r="${state.radius}" 
-    strokeWidth="${state.sw}" 
-    stroke="${state.stroke}"
-    ${showSDR ? `stroke-dasharray="${state.sDR} 1400"` : ""} 
-    />
+          code={`${styleStr}<svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+  <circle 
+    cx="100" cy="100" fill="none"
+    ${svgStr}  />
 </svg>`}
-          language="javascript"
+          language="html"
           theme={prismTheme}
         >
           {({ className, style, tokens, getLineProps, getTokenProps }) => (
